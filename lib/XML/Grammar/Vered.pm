0253f67d69d541dc5e450df4bfe6e1400bac30e1
Shlomi Fish <shlomif@shlomifish.org>
2014-02-17 13:24:32 +0000

    Import of SHLOMIF/XML-Grammar-Vered-0.0.9 from CPAN.
    gitpan-cpan-distribution: XML-Grammar-Vered
    gitpan-cpan-version:      0.0.9
    gitpan-cpan-path:         SHLOMIF/XML-Grammar-Vered-0.0.9.tar.gz
    gitpan-cpan-author:       SHLOMIF
    gitpan-cpan-maturity:     released

--- lib/XML/Grammar/Vered.pm
+++ lib/XML/Grammar/Vered.pm
@@ -12,7 +12,7 @@ use MooX qw/late/;
 use XML::GrammarBase::Role::RelaxNG v0.2.2;
 use XML::GrammarBase::Role::XSLT v0.2.2;
 
-our $VERSION = '0.0.8';
+our $VERSION = '0.0.9';
 
 with ('XML::GrammarBase::Role::RelaxNG');
 with XSLT(output_format => 'docbook');
@@ -41,7 +41,7 @@ XML::Grammar::Vered - a vered by any other name will translate as sweet.
 
 =head1 VERSION
 
-version 0.0.8
+version 0.0.9
 
 =head1 SYNOPSIS
 
